f2049a9dd01fb545aa26d164dc5df4273ed14b66
lpraat <pratissolil@gmail.com>
2018-01-31 10:44:29 +0100

    Added com-redis pod command

--- k8s/kubernetes_deploy.sh
+++ k8s/kubernetes_deploy.sh
@@ -33,4 +33,10 @@ kubectl create -f ./yamls/kong/kong_postgres.yaml
 # KONG SETUP LIKE IN KONG_SETUP.SH #
 ##     ##
 
+
+# Find the redis pod name
+redis_pod=$(kubectl get pods | grep com-redis | awk '{print $1}')
+
+kubectl exec $redis_pod redis-cli config set stop-writes-on-bgsave-error no
+kubectl exec $redis_pod sysctl -p /etc/sysctl.conf
 kubectl delete -f ./yamls/kong/kong_migration_postgres.yaml
